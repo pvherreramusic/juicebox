5930aacc7ea217887580a4def28670ff968b81b7
Patrick-Vincent Herrera <patrick-vincentherrera@Patrick-Vincents-MacBook-Air.local>
2020-05-16 15:45:54 -0700

    Finsihed Code

--- db/index.js
+++ db/index.js
@@ -157,28 +157,30 @@ async function updatePost(postId, fields = {}) {
   }
 }
 
-  async function getAllPosts() {
-    try {
-      const { rows: postIds } = await client.query(`
-        SELECT id
-        FROM posts;
-      `);
-  
-      const posts = await Promise.all(postIds.map(
-        post => getPostById( post.id )
-      ));
-  
-      return posts;
-    } catch (error) {
-      throw error;
-    }
-  }
+
 async function getPostsByUser(userId) {
   try {
     const { rows: postIds } = await client.query(`
       SELECT id 
       FROM posts 
       WHERE "authorId"=${ userId };
+    `);
+
+    const posts = await Promise.all(postIds.map(
+      post => getPostById( post.id )
+    ));
+
+    return posts;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllPosts() {
+  try {
+    const { rows: postIds } = await client.query(`
+      SELECT id
+      FROM posts;
     `);
 
     const posts = await Promise.all(postIds.map(
